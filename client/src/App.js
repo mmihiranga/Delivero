38ded4863130311d92d0fb11fdc4f9f63904c933
Sachintha Muramudalige <it19046594@my.sliit.lk>
2022-08-29 15:35:51 +0530

    protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import NavBar from './components/NavBar';
 import Login from './components/Login';
 import Register from './components/Register';
+import Protected from './Protected';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const user = useSelector((state) => state.user)
+  const [userDetails, setUserDetails] = useState()
+
+  useEffect(() => {
+    if (user.value.length > 0) {
+      console.log("user.value.", user.value)
+      setUserDetails(user.value[0])
+    }
+  }, [user]);
+
+
   return (
     <>
       <NavBar />
       <Routes>
         <Route >
-          <Route path="/" index element={<Home />} />
+          <Route path='/' index
+            element={
+              <Protected isLoggedIn={userDetails}>
+                <Home />
+              </Protected>
+            }
+          />
+          {/* <Route path="/" index element={<Home />} /> */}
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Route>
---
+++ client/src/Protected.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import { Navigate } from "react-router-dom";
+
+const Protected = ({ isLoggedIn, children }) => {
+
+    if (!isLoggedIn) {
+        return <Navigate to="/login" replace />;
+    }
+    return children;
+};
+export default Protected;
